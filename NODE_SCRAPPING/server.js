d6e6836bcbbd88dcee448c2adbbaf6fce8d92af9
martinPeralbo <peralbom98@gmail.com>
2025-01-29 15:35:10 +0100

    añadido precio maximo y minimo

--- NODE_SCRAPPING/server.js
+++ NODE_SCRAPPING/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const { chromium } = require('playwright');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+
+const dataFilePath = path.join(__dirname, 'gameData.json');
 
 app.get('/', async (req, res) => {
     const browser = await chromium.launch();
@@ -23,7 +27,7 @@ app.get('/', async (req, res) => {
             return {
                 url: linkElement ? linkElement.href : null,  // URL del juego
                 name: nameElement ? nameElement.textContent.trim() : null, // Nombre del juego
-                price: priceElement ? priceElement.textContent.trim() : null, // Precio del juego
+                price: priceElement ? parseFloat(priceElement.textContent.trim().replace('€', '')) : null, // Precio del juego
                 imageUrl: imageElement ? imageElement.getAttribute('src') : null // URL de la imagen
             };
         });
@@ -31,7 +35,33 @@ app.get('/', async (req, res) => {
 
     await browser.close();
 
-    res.render('index', { results: gameLinks });
+    // Leer datos anteriores del fichero
+    let previousData = {};
+    if (fs.existsSync(dataFilePath)) {
+        const rawData = fs.readFileSync(dataFilePath);
+        previousData = JSON.parse(rawData);
+    }
+
+    // Actualizar datos con la información actual
+    const updatedData = gameLinks.map(game => {
+        const previousGameData = previousData[game.name] || { minPrice: game.price, maxPrice: game.price, url: game.url, imageUrl: game.imageUrl };
+        return {
+            ...game,
+            minPrice: Math.min(game.price, previousGameData.minPrice),
+            maxPrice: Math.max(game.price, previousGameData.maxPrice),
+            url: game.url || previousGameData.url,
+            imageUrl: game.imageUrl || previousGameData.imageUrl
+        };
+    });
+
+    // Guardar datos actualizados en el fichero
+    const dataToSave = updatedData.reduce((acc, game) => {
+        acc[game.name] = game;
+        return acc;
+    }, {});
+    fs.writeFileSync(dataFilePath, JSON.stringify(dataToSave, null, 2));
+
+    res.render('index', { results: updatedData });
 });
 
 app.listen(port, () => {
